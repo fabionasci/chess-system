110d3551169baafca0be9dcde22698fc7999b15d
Fabio Nascimento <nascimento.fab@gmail.com>
2025-11-02 15:44:25 -0300

    First commit

---
+++ src/main/java/com/curso/java/completo/chesssystem/ChessSystemApplication.java
@@ -0,0 +1,13 @@
+package com.curso.java.completo.chesssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/curso/java/completo/chesssystem/ChessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.java.completo.chesssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
