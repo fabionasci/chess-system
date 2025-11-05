2e0c6a26273bd5d8d033cefdab6ac368369f127a
Fabio Nascimento <nascimento.fab@gmail.com>
2025-11-05 01:15:03 -0300

    Pom adjustments and UI implementation

--- src/main/java/com/curso/java/completo/chesssystem/ChessSystemApplication.java
+++ src/main/java/com/curso/java/completo/chesssystem/ChessSystemApplication.java
@@ -1,20 +1,12 @@
 package com.curso.java.completo.chesssystem;
 
-import com.curso.java.completo.chesssystem.core.board.Board;
-import com.curso.java.completo.chesssystem.core.board.Piece;
-import com.curso.java.completo.chesssystem.core.board.Position;
+import com.curso.java.completo.chesssystem.core.chess.ChessMatch;
 
 public class ChessSystemApplication {
 
     public static void main(String[] args) {
 
-        Position position = new Position(2, 2);
-        System.out.println("Teste position: " + position);
-
-        Board board = new Board(8, 8);
-        System.out.println("Tabuleiro criado com " + board.getRows() + " linhas e " + board.getColumns() + " colunas.");
-
-        Piece piece = new Piece(board);
-        System.out.println("Peça criada para o tabuleiro: " + piece);
+        ChessMatch match = new ChessMatch();
+        UI.printBoard(match.getPieces());
     }
 }
--- src/main/java/com/curso/java/completo/chesssystem/UI.java
+++ src/main/java/com/curso/java/completo/chesssystem/UI.java
@@ -72,6 +72,7 @@ public class UI {
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
+		System.out.println();
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
