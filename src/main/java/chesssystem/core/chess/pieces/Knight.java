3e1c378e7b8beb21d4bbba240e8bef3057b80996
Fabio Nascimento <nascimento.fab@gmail.com>
2025-11-14 00:39:12 -0300

    Knight

--- src/main/java/chesssystem/core/chess/pieces/Knight.java
+++ src/main/java/chesssystem/core/chess/pieces/Knight.java
@@ -1,6 +1,7 @@
 package chesssystem.core.chess.pieces;
 
 import chesssystem.core.board.Board;
+import chesssystem.core.board.Position;
 import chesssystem.core.chess.ChessPiece;
 import chesssystem.core.chess.enums.Color;
 
@@ -19,6 +20,28 @@ public class Knight extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        int[][] moves = {
+                {-2, -1}, {-2, +1},
+                {-1, -2}, {-1, +2},
+                {+1, -2}, {+1, +2},
+                {+2, -1}, {+2, +1}
+        };
+
+        for (int[] move : moves) {
+            p.setValues(position.getRow() + move[0], position.getColumn() + move[1]);
+            if (getBoard().positionExists(p) && canMove(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
+
         return mat;
+    }
+
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
     }
 }
