b7d15c41d9ca627cd431b694cb319d426c1e519b
Fabio Nascimento <nascimento.fab@gmail.com>
2025-11-15 02:38:36 -0300

    Last adjustments

--- src/main/java/core/chess/ChessMatch.java
+++ src/main/java/core/chess/ChessMatch.java
@@ -7,7 +7,6 @@ import core.chess.enums.Color;
 import core.chess.exceptions.ChessException;
 import core.chess.pieces.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -122,7 +121,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-            throw new InvalidParameterException("Invalid promoted piece type");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
